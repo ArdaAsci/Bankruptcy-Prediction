3c7d547e3e457d9d8559c72decf341aa9cf7c34d
arda.asci <abdullah-arda@hotmail.com>
2021-05-06 21:11:43 +0300

    pca done

--- asd.py
+++ asd.py
@@ -1,57 +1,107 @@
+from __future__ import division, print_function
 import numpy as np
-import numpy.linalg as LA
-import sklearn
-from sklearn.neighbors import NearestNeighbors
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-import random
+import progressbar
 
-class Smote(object):
+# Import helper functions
+from mlfromscratch.utils import train_test_split, standardize, to_categorical
+from mlfromscratch.utils import mean_squared_error, accuracy_score
+from mlfromscratch.deep_learning.loss_functions import SquareLoss, CrossEntropy
+from mlfromscratch.supervised_learning.decision_tree import RegressionTree
+from mlfromscratch.utils.misc import bar_widgets
 
-    def __init__(self, minority_samples):
-        self.samples = minority_samples
-        pass
 
-    def oversample(self, N: int = 500, k: int = 5):
-        N = N//100
-        numsamples, numattrs = self.samples.shape
-        synthetic = np.zeros( (N*numsamples, numattrs))
+class GradientBoosting(object):
+    """Super class of GradientBoostingClassifier and GradientBoostinRegressor. 
+    Uses a collection of regression trees that trains on predicting the gradient
+    of the loss function. 
+    Parameters:
+    -----------
+    n_estimators: int
+        The number of classification trees that are used.
+    learning_rate: float
+        The step length that will be taken when following the negative gradient during
+        training.
+    min_samples_split: int
+        The minimum number of samples needed to make a split when building a tree.
+    min_impurity: float
+        The minimum impurity required to split the tree further. 
+    max_depth: int
+        The maximum depth of a tree.
+    regression: boolean
+        True or false depending on if we're doing regression or classification.
+    """
+    def __init__(self, n_estimators, learning_rate, min_samples_split,
+                 min_impurity, max_depth, regression):
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.min_samples_split = min_samples_split
+        self.min_impurity = min_impurity
+        self.max_depth = max_depth
+        self.regression = regression
+        self.bar = progressbar.ProgressBar(widgets=bar_widgets)
         
-        matrix=[]
-        indices = k_NN(self.samples)
-        newindex = 0
-        for i in range(numsamples):
-            while N > 0:
-                nn = random.randint(0, k)
-                for attr in range(numattrs):
-                    dif = self.samples[indices[i, nn], attr] -self.samples[i, attr]
-                    gap = random.uniform(0,1)
-                    synthetic[newindex,attr] = self.samples[i, attr] + gap*dif
-                newindex += 1
-                N -= 1
-        return synthetic
-        #for m in range(len(indices)):
-        #    t=self.samples[indices[m]]
-        #    newt=pd.DataFrame(t)
-        #    matrix.append([])
-        #    for j in range(len(newt.columns)):
-        #        matrix[m].append(random.choice(newt[j]))
-        return matrix
+        # Square loss for regression
+        # Log loss for classification
+        self.loss = SquareLoss()
+        if not self.regression:
+            self.loss = CrossEntropy()
 
-def k_NN(X: np.ndarray, k: int = 5):
-    indices_matrix = np.zeros((X.shape[0], k))
-    for idx, row in enumerate(X):
-        dist = LA.norm(X-row, axis=1)
-        indices = np.argsort(dist)[:k]
-        indices_matrix[idx,:] = indices 
-    return indices_matrix.astype(int)  
+        # Initialize regression trees
+        self.trees = []
+        for _ in range(n_estimators):
+            tree = RegressionTree(
+                    min_samples_split=self.min_samples_split,
+                    min_impurity=min_impurity,
+                    max_depth=self.max_depth)
+            self.trees.append(tree)
 
-raw_data = pd.read_csv("data.csv")
-bankrupt_pd = raw_data["Bankrupt?"]
-features_pd = raw_data.drop(["Bankrupt?"], axis=1)
-bankrupt = bankrupt_pd.to_numpy()
 
-minority = features_pd[bankrupt_pd == 1]
-smote = Smote(minority.to_numpy())
-smote.oversample()
+    def fit(self, X, y):
+        y_pred = np.full(np.shape(y), np.mean(y, axis=0))
+        for i in self.bar(range(self.n_estimators)):
+            gradient = self.loss.gradient(y, y_pred)
+            self.trees[i].fit(X, gradient)
+            update = self.trees[i].predict(X)
+            # Update y prediction
+            y_pred -= np.multiply(self.learning_rate, update)
+
+
+    def predict(self, X):
+        y_pred = np.array([])
+        # Make predictions
+        for tree in self.trees:
+            update = tree.predict(X)
+            update = np.multiply(self.learning_rate, update)
+            y_pred = -update if not y_pred.any() else y_pred - update
+
+        if not self.regression:
+            # Turn into probability distribution
+            y_pred = np.exp(y_pred) / np.expand_dims(np.sum(np.exp(y_pred), axis=1), axis=1)
+            # Set label to the value that maximizes probability
+            y_pred = np.argmax(y_pred, axis=1)
+        return y_pred
+
+
+class GradientBoostingRegressor(GradientBoosting):
+    def __init__(self, n_estimators=200, learning_rate=0.5, min_samples_split=2,
+                 min_var_red=1e-7, max_depth=4, debug=False):
+        super(GradientBoostingRegressor, self).__init__(n_estimators=n_estimators, 
+            learning_rate=learning_rate, 
+            min_samples_split=min_samples_split, 
+            min_impurity=min_var_red,
+            max_depth=max_depth,
+            regression=True)
+
+class GradientBoostingClassifier(GradientBoosting):
+    def __init__(self, n_estimators=200, learning_rate=.5, min_samples_split=2,
+                 min_info_gain=1e-7, max_depth=2, debug=False):
+        super(GradientBoostingClassifier, self).__init__(n_estimators=n_estimators, 
+            learning_rate=learning_rate, 
+            min_samples_split=min_samples_split, 
+            min_impurity=min_info_gain,
+            max_depth=max_depth,
+            regression=False)
+
+    def fit(self, X, y):
+        y = to_categorical(y)
+        super(GradientBoostingClassifier, self).fit(X, y)

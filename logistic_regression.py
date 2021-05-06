2798944622fa7d8b601f4d71019edca11e098dd5
klatyma <ardaturkyasar@gmail.com>
2021-05-06 22:04:27 +0300

    logistic reg

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,37 +3,35 @@ import numpy as np
 
 class logistic_regression(object):
 
+    def __init__(self, size) -> None:
+        self.b = np.random.rand(size,1)
+        pass
 
-    def sigmoid(z):
-    sig = 1.0 / (1 + np.exp(-z))
-    return sig
+    def sigmoid(z:float):
+        sig = 1.0 / (1 + np.exp(-z))
+        return sig
 
-    def pred(x,b):
-        y = np.dot(x,b)
-        return sigmoid(y)
+    def pred(features, weight):
+        y = features@weight
+        return logistic_regression.sigmoid(y)
 
 
     def classify(prediction, boundary=0.5):
-        if prediction <= boundary:
-            return 0
-        else:
-            return 1
+        return prediction > boundary
 
-    def cost_func(features, labels, b):
-        prediction = pred(features,b)
+    def cost_func(self, features:np.ndarray, labels):
+        prediction = logistic_regression.pred(features,self.b)
         cost = (-labels*np.log(prediction)-(1-labels)*np.log(1-prediction))
         average = cost.sum() / len(features)
         return average
 
-    def gradient_descent(features, labels, b, learning_rate = 0.2, depth = 1000):
-        prediction = pred(features, b)
-        gradient = np.dot(features.T,  prediction - labels)
+    def gradient_descent( features:np.ndarray, labels, weight, learning_rate = 0.02):
+        prediction = logistic_regression.pred(features, weight)
+        gradient = features.T@ (prediction - labels)
         gradient = gradient / len(features)
         gradient = gradient * learning_rate
-        b = b - gradient
-        for i in range(depth):
-            b = gradient_descent(features, labels, b)
-        return b
+        weight = weight - gradient
+        return weight
     
 
     #weights = logistic_regression.gradient_descent()
